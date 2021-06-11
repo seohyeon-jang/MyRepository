a4e7d72dc0ccb7300a1eb790ee5fe71d2ba22abb
wkdtj <wkdtj@DESKTOP-7FQOC4M>
2021-06-11 13:45:49 +0900

    change in master branch

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -3,5 +3,7 @@ public class MyClass {
 	public static void main(String args[]) {
 			System.out.println("Hello Git!");
 			System.out.println("This is 2nd version");
+			newMyHello().sayHello();
+			System.out.println("Change in master branch");
 	}
 }
