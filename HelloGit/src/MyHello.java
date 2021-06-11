06499f68daf4c38cdb5b44303eeb111247ca50bb
wkdtj <wkdtj@DESKTOP-7FQOC4M>
2021-06-11 13:36:23 +0900

    First commit in dev branch

--- HelloGit/src/MyHello.java
+++ HelloGit/src/MyHello.java
@@ -1,6 +1,8 @@
 
 public class MyHello {
 	public void sayHello() {
-			System.out.println("Hello!");
+			System.out.println("Hello1");
+			System.out.println("Hello2");
+			System.out.println("Hello3");
 	}
 }
